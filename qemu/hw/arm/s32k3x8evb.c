ba3c250460a4b3f8c5020ab6cab4e932d5a34bda
aaldat <alessio.flamini@hotmail.it>
2025-08-31 11:16:34 +0200

    Fix errore s32k3x8evb.c, ma ora segmentation fault

--- qemu/hw/arm/s32k3x8evb.c
+++ qemu/hw/arm/s32k3x8evb.c
@@ -12,10 +12,9 @@
 #include "exec/memory.h"
 #include "exec/address-spaces.h"
 
-#include "hw/char/stm32l4x5_usart.h"
-
 #include "hw/char/serial.h"
 #include "hw/qdev-properties.h"
+#include "hw/char/stm32l4x5_usart.h"
 
 #include "hw/core/split-irq.h"
 #include "hw/sysbus.h"
@@ -28,25 +27,25 @@
 #include "migration/vmstate.h"
 #include "qom/object.h"
 
-#define KiB                1024ULL
-#define MiB               (1024ULL * KiB)
+#define KiB                   (1024ULL)
+#define MiB                   (1024ULL * KiB)
 
-#define FLASH_SIZE         (8 * MiB)
-#define FLASH_OFFSET       0x00400000ULL
+#define FLASH_SIZE            (8 * MiB)
+#define FLASH_OFFSET          0x00400000ULL
 
-#define RAM_SIZE           (768 * KiB)
-#define RAM_OFFSET         0x20400000ULL
+#define RAM_SIZE              (768 * KiB)
+#define RAM_OFFSET            0x20400000ULL
 
-#define ITCM_SIZE          (64 * KiB)
-#define ITCM_OFFSET        0x00000000ULL
+#define ITCM_SIZE             (64 * KiB)
+#define ITCM_OFFSET           0x00000000ULL
 
-#define DFLASH_SIZE        (128 * KiB)
-#define DFLASH_OFFSET      0x10000000ULL
+#define DFLASH_SIZE           (128 * KiB)
+#define DFLASH_OFFSET         0x10000000ULL
 
-#define DTCM_SIZE          (128 * KiB)
-#define DTCM_OFFSET        0x20000000ULL
+#define DTCM_SIZE             (128 * KiB)
+#define DTCM_OFFSET           0x20000000ULL
 
-#define UART_BASE_ADDR     0x4006A000ULL
+#define UART_BASE_ADDR        0x4006A000ULL
 
 #define FLASH_BLOCK0_BASE_ADDR FLASH_OFFSET
 #define FLASH_BLOCK0_SIZE      (2 * MiB)
@@ -58,6 +57,11 @@
  * System Controller (ssys_state)
  *================================================================*/
 #define TYPE_S32K3X8EVB_SYS "s32k3x8evb-sys"
+
+typedef struct S32K3X8EVBSysClass {
+    SysBusDeviceClass parent_class;
+} S32K3X8EVBSysClass;
+
 OBJECT_DECLARE_SIMPLE_TYPE(ssys_state, S32K3X8EVB_SYS);
 
 typedef struct ssys_state {
@@ -68,15 +72,13 @@ typedef struct ssys_state {
     Clock *aips_slow_clk;
 } ssys_state;
 
-static void s32k3x8evb_sys_class_init(ObjectClass *oc, void *data)
-{
-    /* Nessuna proprietà aggiuntiva */
-}
+static void s32k3x8evb_sys_class_init(ObjectClass *oc, void *data) { }
 
 static const TypeInfo s32k3x8evb_sys_info = {
     .name          = TYPE_S32K3X8EVB_SYS,
     .parent        = TYPE_SYS_BUS_DEVICE,
     .instance_size = sizeof(ssys_state),
+    .class_size    = sizeof(S32K3X8EVBSysClass),
     .class_init    = s32k3x8evb_sys_class_init,
 };
 
@@ -84,6 +86,13 @@ static const TypeInfo s32k3x8evb_sys_info = {
  * SoC Memory Device (S32K3State)
  *================================================================*/
 #define TYPE_S32K3 "s32k3"
+
+typedef struct S32K3Class {
+    SysBusDeviceClass parent_class;
+    uint32_t flash_size;
+    uint32_t ram_size;
+} S32K3Class;
+
 OBJECT_DECLARE_SIMPLE_TYPE(S32K3State, S32K3);
 
 typedef struct S32K3State {
@@ -94,12 +103,6 @@ typedef struct S32K3State {
     MemoryRegion dflash;
     MemoryRegion dtcm;
 } S32K3State;
-
-typedef struct S32K3Class {
-    DeviceClass parent_class;
-    uint32_t flash_size;
-    uint32_t ram_size;
-} S32K3Class;
 
 #define S32K3_GET_CLASS(obj) \
     OBJECT_GET_CLASS(S32K3Class, obj, TYPE_S32K3)
@@ -153,15 +156,16 @@ static const TypeInfo s32k3_info = {
 /*================================================================
  * Machine State
  *================================================================*/
+/* interno QOM type: DEVE finire con “-machine” */
+#define TYPE_S32K3X8_MACHINE "s32k3x8evb-machine"
+
+OBJECT_DECLARE_SIMPLE_TYPE(S32K3X8MachineState, S32K3X8_MACHINE);
+
 typedef struct S32K3X8MachineState {
-    MachineState *parent_obj;
+    MachineState parent_obj;
     ssys_state sys;
 } S32K3X8MachineState;
 
-#define TYPE_S32K3X8_MACHINE MACHINE_TYPE_NAME("s32k3x8evb")
-DECLARE_INSTANCE_CHECKER(S32K3X8MachineState, S32K3X8_MACHINE, TYPE_S32K3X8_MACHINE);
-
-/* UART Initialization */
 static void initialize_lpuarts(S32K3X8MachineState *m_state,
                                DeviceState *nvic, int num_lpuarts)
 {
@@ -186,58 +190,55 @@ static void initialize_lpuarts(S32K3X8MachineState *m_state,
     }
 }
 
-/* Machine Initialization */
 static void s32k3x8_init(MachineState *ms)
 {
-    S32K3X8MachineState *m = g_new0(S32K3X8MachineState, 1);
-    m->parent_obj = ms;
+    S32K3X8MachineState *m = S32K3X8_MACHINE(ms);
 
-    /* SoC container */
+    /* container SoC */
     Object *soc = object_new("container");
-    object_property_add_child(OBJECT(ms), "soc", soc);
+    object_property_add_child(OBJECT(m), "soc", soc);
 
-    /* System controller */
-    object_initialize_child(OBJECT(ms), "sys", &m->sys,
+    /* system controller */
+    object_initialize_child(OBJECT(m), "sys", &m->sys,
                             TYPE_S32K3X8EVB_SYS);
     sysbus_realize(SYS_BUS_DEVICE(&m->sys), &error_abort);
 
-    /* Memoria SoC */
+    /* SoC memory device */
     DeviceState *mem = qdev_new(TYPE_S32K3);
     object_property_add_child(soc, "mem", OBJECT(mem));
     sysbus_realize_and_unref(SYS_BUS_DEVICE(mem), &error_fatal);
     s32k3_memory(mem, &error_fatal);
 
-    /* Clocks */
+    /* clocks */
     m->sys.sysclk        = clock_new(OBJECT(DEVICE(&m->sys)), "sysclk");
     clock_set_ns(m->sys.sysclk, 4.16);
-
     m->sys.refclk        = clock_new(OBJECT(DEVICE(&m->sys)), "refclk");
     clock_set_hz(m->sys.refclk, 1000000);
-
-    m->sys.aips_plat_clk = clock_new(OBJECT(DEVICE(&m->sys)), "aips_plat_clk");
+    m->sys.aips_plat_clk = clock_new(OBJECT(DEVICE(&m->sys)),
+                                     "aips_plat_clk");
     clock_set_hz(m->sys.aips_plat_clk, 80000000);
-
-    m->sys.aips_slow_clk = clock_new(OBJECT(DEVICE(&m->sys)), "aips_slow_clk");
+    m->sys.aips_slow_clk = clock_new(OBJECT(DEVICE(&m->sys)),
+                                     "aips_slow_clk");
     clock_set_hz(m->sys.aips_slow_clk, 40000000);
 
     /* NVIC / Cortex-M7 */
     DeviceState *nvic = qdev_new(TYPE_ARMV7M);
     object_property_add_child(soc, "nvic", OBJECT(nvic));
     qdev_prop_set_uint32(nvic, "num-irq",       256);
-    qdev_prop_set_uint8 (nvic, "num-prio-bits", 4);
-    qdev_connect_clock_in(nvic, "cpuclk", m->sys.sysclk);
-    qdev_connect_clock_in(nvic, "refclk", m->sys.refclk);
+    qdev_prop_set_uint8(nvic, "num-prio-bits",  4);
+    qdev_connect_clock_in(nvic, "cpuclk",       m->sys.sysclk);
+    qdev_connect_clock_in(nvic, "refclk",       m->sys.refclk);
     qdev_prop_set_string(nvic, "cpu-type",      ms->cpu_type);
-    qdev_prop_set_bit   (nvic, "enable-bitband", true);
+    qdev_prop_set_bit(nvic, "enable-bitband",   true);
     object_property_set_link(OBJECT(nvic), "memory",
                              OBJECT(get_system_memory()),
                              &error_abort);
     sysbus_realize_and_unref(SYS_BUS_DEVICE(nvic), &error_fatal);
 
-    /* UARTs */
+    /* UART */
     initialize_lpuarts(m, nvic, 16);
 
-    /* Carica firmware in FLASH */
+    /* firmware in FLASH */
     armv7m_load_kernel(ARM_CPU(first_cpu),
                        ms->kernel_filename,
                        FLASH_BLOCK0_BASE_ADDR,
@@ -247,10 +248,10 @@ static void s32k3x8_init(MachineState *ms)
                        FLASH_BLOCK3_SIZE);
 }
 
-/* MachineClass registration */
 static void s32k3x8_class_init(ObjectClass *oc, void *data)
 {
     MachineClass *mc = MACHINE_CLASS(oc);
+
     mc->name             = g_strdup("s32k3x8evb");
     mc->desc             = "NXP S32K3X8 EVB (Cortex-M7)";
     mc->init             = s32k3x8_init;
@@ -264,16 +265,17 @@ static void s32k3x8_class_init(ObjectClass *oc, void *data)
 }
 
 static const TypeInfo s32k3x8_machine_info = {
-    .name       = TYPE_S32K3X8_MACHINE,
-    .parent     = TYPE_MACHINE,
-    .class_init = s32k3x8_class_init,
+    .name          = TYPE_S32K3X8_MACHINE,
+    .parent        = TYPE_MACHINE,
+    .instance_size = sizeof(S32K3X8MachineState),
+    .class_size    = sizeof(MachineClass),
+    .class_init    = s32k3x8_class_init,
 };
 
 static void s32k3x8evb_machine_init(void)
 {
-    type_register_static(&s32k3x8_machine_info);
     type_register_static(&s32k3x8evb_sys_info);
     type_register_static(&s32k3_info);
+    type_register_static(&s32k3x8_machine_info);
 }
-
 type_init(s32k3x8evb_machine_init);
